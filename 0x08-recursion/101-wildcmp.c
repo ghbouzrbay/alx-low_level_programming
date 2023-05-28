27580a6c99084443ff08412d28c6ab462b929b9b
bouzrbay <atighmarcelline@gmail.com>
2023-05-28 20:25:57 +0100

    advanced

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *wildcmp - campares two strings
+ *
+ *@s1: string 1
+ *@s2: string 2 it can contains a * as a special character
+ *
+ *Return: 1 if are identical, 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && *s2 == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (*s2 == '\0');
+}
+
+if (*s2 == '*')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+
+return (0);
+
+}
