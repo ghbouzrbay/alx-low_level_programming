9664a093ce0d7bf8bbd6ed3f1e9960f941ea2b3f
bouzrbay <atighmarcelline@gmail.com>
2023-05-30 14:34:19 +0100

    tasks

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlb.h>
+#include <string.h>
+#include <limits.h>
+
+
+/*********FUNCTIONS**********/
+
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+#endif
