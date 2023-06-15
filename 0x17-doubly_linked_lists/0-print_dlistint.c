a12b29f0cc776c679cdfa38a3b936878ab124e82
bouzrbay <atighmarcelline@gmail.com>
2023-06-15 19:14:36 +0100

    correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,22 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - function takes the head of
+ *the doubly linked list
+ *
+ *@h: ...
+ *
+ *Return: ...
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t node_count = 0;
-    
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
-    
-    return node_count;
+size_t node_count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+node_count++;
+}
+return (node_count);
 }
