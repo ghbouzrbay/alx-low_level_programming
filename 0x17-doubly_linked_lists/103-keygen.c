86bdd73516466773aa5d2d53e7b2d05f79d8fa8a
bouzrbay <atighmarcelline@gmail.com>
2023-06-15 21:01:21 +0100

    103

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *  generateKey - ...
+ *
+ * @username: ...
+ *
+ */
+
+void generateKey(char *username)
+{
+printf("Key for %s: %s\n", username, generatedKey);
+}
+
+/**
+ * main - ...
+ *
+ * @argc: ...
+ * @argv: ...
+ *
+ * Return: ...
+ */
+
+int main(int argc, char *argv[])
+{
+char *username = argv[1]; 
+if (argc != 2)
+ {
+printf("Usage: %s username\n", argv[0]);
+return (1);
+}
+
+generateKey(username);
+
+return (0);
+}
