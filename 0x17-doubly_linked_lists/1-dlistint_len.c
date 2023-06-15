6bf7ff902ea2e3c470a562cacc8f075c79af39df
bouzrbay <atighmarcelline@gmail.com>
2023-06-15 19:21:56 +0100

    task_1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+ * dlistint_len - function takes the head of
+ * the doubly linked list
+ *
+ * @h: argument and iterates
+ *
+ * Return: ...
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t node_count = 0;
+
+while (h != NULL)
+{
+node_count++;
+h = h->next;
+}
+    
+return (node_count);
+}
