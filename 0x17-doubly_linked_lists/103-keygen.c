2716917c33382f64cd60659555486be43199c154
bouzrbay <atighmarcelline@gmail.com>
2023-06-16 00:21:35 +0100

    correct

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -9,7 +9,7 @@
  *
  * Return: Always 0.
  */
-int main(int _attribute((unused_)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 char password[7], *codex;
 int len = strlen(argv[1]), i, tmp;
