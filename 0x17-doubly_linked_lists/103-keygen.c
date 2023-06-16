4367145bee62deea170f55a2a2ba6e7f9fd1c13b
bouzrbay <atighmarcelline@gmail.com>
2023-06-16 03:06:23 +0100

    change

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -10,7 +10,7 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 char password[7];
 char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
