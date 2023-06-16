aeafc0bc933e69bd25aa94f99d9adb8eff28a7c9
bouzrbay <atighmarcelline@gmail.com>
2023-06-16 03:09:16 +0100

    betty_style

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -13,23 +13,20 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 char password[7];
-char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+char *charset;
+charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 int input_length = strlen(argv[1]);
 int i, temp;
-
 temp = (input_length ^ 59) & 63;
 password[0] = charset[temp];
-
 temp = 0;
 for (i = 0; i < input_length; i++)
 temp += argv[1][i];
 password[1] = charset[(temp ^ 79) & 63];
-
 temp = 1;
 for (i = 0; i < input_length; i++)
 temp *= argv[1][i];
 password[2] = charset[(temp ^ 85) & 63];
-
 temp = 0;
 for (i = 0; i < input_length; i++)
 {
@@ -38,7 +35,6 @@ temp = argv[1][i];
 }
 srand(temp ^ 14);
 password[3] = charset[rand() & 63];
-
 temp = 0;
 for (i = 0; i < input_length; i++)
 temp += (argv[1][i] * argv[1][i]);
@@ -47,10 +43,8 @@ password[4] = charset[(temp ^ 239) & 63];
 for (i = 0; i < argv[1][0]; i++)
 temp = rand();
 password[5] = charset[(temp ^ 229) & 63];
-
 password[6] = '\0';
 
 printf("%s\n", password);
-
 return (0);
 }
