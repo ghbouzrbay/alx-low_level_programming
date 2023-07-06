e20441dd6cc156505132e5a34070c49bd82ed8c1
bouzrbay <atighmarcelline@gmail.com>
2023-07-06 21:31:06 +0100

    correct

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -174,7 +174,7 @@ void shash_table_print(const shash_table_t *ht)
     printf("}\n");
 }
 
-B
+
 /**
  * shash_table_print_rev - Prints a sorted hash table in reverse order.
  * @ht: A pointer to the sorted hash table to print.
