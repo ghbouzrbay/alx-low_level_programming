683fbcda8cb23a02bb0bc22e022dce3cc069e627
bouzrbay <atighmarcelline@gmail.com>
2023-08-23 22:02:25 +0100

    MONDATORY

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "makes.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 
+ */
+int main(void)
+{
+	_school();
+	return (EXIT_SUCCESS);
+}
---
+++ 0x1C-makefiles/makes.h
@@ -0,0 +1,9 @@
+#ifndef __MAKES_H__
+#define __MAKES_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void _school(void);
+
+#endif
