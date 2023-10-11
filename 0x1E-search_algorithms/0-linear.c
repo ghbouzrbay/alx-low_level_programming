2a6a80074c2cfadbbb0bf1063a1ea504df328523
ghbouzrbay <atighmarcelline@gmail.com>
2023-10-11 14:39:38 -0700

    deleteline

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,8 +7,9 @@
  * @size: number of elements in array.
  * @value: value to search for.
  *
- * Return: first index where value is located.
- **/
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the first index where the value is located.
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
